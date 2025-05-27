2539cad60eb3c7cdee4834f89444c0c293fd1a55
Eddo <eddomora@gmail.com>
2025-05-27 14:47:27 -0400

    Creacion de base para la carpeta visual

--- Proyecto3/src/main/java/org/example/Visual/Ventana.java
+++ Proyecto3/src/main/java/org/example/Visual/Ventana.java
@@ -1,19 +1,23 @@
-package org.example;
+package org.example.Visual;
 import javax.swing.JFrame;
 import java.awt.*;
 //Esto es lo que rescaté de la ayudantía (algunas funciones no se que hacen).
 public class Ventana extends JFrame {
+
     public static final int ANCHO = 1000;
     public static final int ALTO = 800;
     public Ventana instancia;
 
-    private Ventana() {
-        setTitle("Titulooo");
+    public Ventana() {
+        setTitle("DOOM The Expendedor");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(1014,816));
         setSize(1014,816);
         setResizable(false);
         setLocationRelativeTo(null);
+        setVisible(true);
+        Menu_Expendedor me = new Menu_Expendedor();
+        this.add(me,BorderLayout.SOUTH);
         setVisible(true);
     }
 }
